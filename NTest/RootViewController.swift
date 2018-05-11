d2cb1d265b700c7af94379688d9680554070b198
WU YUTONG <wyt363466854@gmail.com>
2018-05-11 23:58:11 +0900

    000

--- NTest/RootViewController.swift
+++ NTest/RootViewController.swift
@@ -103,6 +103,7 @@ class RootViewController: UIViewController {
     segmentedControl = UISegmentedControl(items: ["left","right"])
     self.view.addSubview(segmentedControl)
 
+
     segmentedControl.frame = .zero
     segmentedControl.addTarget(self, action: #selector(controlBottomHide), for: .valueChanged)
   }
